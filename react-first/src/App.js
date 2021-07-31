0724059f12a0a8b5aa77028ed4e9ab3f160cde6e
alexandr <alexandrde@yandex.ru>
2021-07-31 20:05:30 +0400

    end

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
+// import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+// import { useEffect } from "react";
+
+const AddTodo = React.lazy(()=> import('./Todo/AddTodo'))
 
 function App() {
-  const [todos, setTodos] = React.useState([
-      {id: 1, completed:false, title:'Купить хлеб'},
-      {id: 2, completed:true, title:'Купить масло'},
-      {id: 3, completed:false, title:'Купить молоко'}
-    ]
-  )
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading]=React.useState(true)
+
+  // [
+  //     {id: 1, completed:false, title:'Купить хлеб'},
+  //     {id: 2, completed:true, title:'Купить масло'},
+  //     {id: 3, completed:false, title:'Купить молоко'}
+  //   ]
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(()=>{
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
 
   function toggleTodo(id) {
     setTodos(
@@ -40,9 +58,16 @@ function App() {
     <div className='wrapper'>
     
     <h1>react tutorial</h1>
-    <AddTodo onCreate={addTodo} />
-
-    {todos.length ? <TodoList todos={todos} onTougle={toggleTodo}/> : <p>no todos</p>}
+    <Modal/>
+    <React.Suspense fallback={<p>Loading....</p>}>
+      <AddTodo onCreate={addTodo} />
+    </React.Suspense>
+    {loading && <Loader/> }
+    {todos.length ? (
+      <TodoList todos={todos} onTougle={toggleTodo}/>
+    ) : (
+     loading ? null :  <p>no todos</p>
+    )}
     
     </div>
     </Context.Provider>
